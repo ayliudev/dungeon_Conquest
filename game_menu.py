6b7a8c19809ad8bf64aed21dd2f2e3bfef976b83
alice-moegi <52728833+alice-moegi@users.noreply.github.com>
2019-07-10 16:46:04 +0900

    Update game_menu.py

--- game_menu.py
+++ game_menu.py
@@ -1,28 +1,93 @@
-import random
-import time
-#checks if there is save data.
-
 if __name__ == '__main__':
-    print("Welcome to ___")
     try:
         hero = open("/hero.txt")
-        Continue()
-    #no save data, so continues
+        game = True
     except:
-        NewGame()
-    while game = True:
+        #NewGame()
+        print("Welcome to Dungeon Conquest")
+        x = input("Please enter your Character name")
+        player = Hero(x)
+        print(f"Welcome {hero.name}")
+        #Below here are the instructions to the game
+        print("")
+        game = True
+    while game == True:
         print("Menu")
-        print("Enter options '1 - 5':")
+        option = input("Enter options '1 - 5':")
         print("1 - New Game")
         print("2 - Continue")
         print("3 - Save")
         print("4 - Character Data")
         print("5 - Save and Quit")
-        
 
-        if option == 5:
-            # Save and quit
-            game = False
+        
+        if option == "1": # New Game 
+            print("Welcome to Dungeon Conquest")
+            x = input("Please enter your Character name")
+            player = Hero(x)
+            print(f"Welcome {hero.name}")
+            #Below here are the instructions to the game
+            print("")
+            game = True
+                
+        elif option == "2": # Continue
+            hero_name = input("What's your hero's name?")
+            print("Welcome back, ", hero_name)
+            # retrieve data from saved file
+            open_hero(hero_name)
+               
+            # display game menu
+            hero = True
+            while hero == True:
+                option = input("Enter options 1 - 4:")
+                print("1 - Battle")
+                print("2 - Main Menu")
+                print("3 - Character Information")
+                
+                if option == "1": # Battle
+
+
+                    ## TBC ##
+
+                                  
+                elif option == "2": # Back to main menu
+                    hero = False
+                    game = True
+                                  
+                elif option == "3": # Display Character Information
+                    CharacterData(hero.name)
+
+                    ## ??? ##
+                                  
+                else:
+                    print("Please enter valid option, e.g. 1, 2 etc.")
+                
+            
+        elif option == "3": # Save
+
+
+            ##TBC##
+
+
+        elif option == "4": # Display Character Data
+
+
+            ## ??? ##
+            
+
+        elif option == "5": # Save and quit
+            game = False # Quit the loop
+            
+        else: # Invalid input
+            print("Please enter valid option, e.g. 1, 2 etc.")
+            game = True
+
+
+    # shelve the data
+    Save(player)
+
+
+    ## TBC ##
             
 
 
@@ -30,13 +95,3 @@ if __name__ == '__main__':
 
     
 
-def NewGame():
-    print("Welcome to ____")
-    print("Please enter your Character name")
-    x = str(input())
-    player = Hero(x)
-    print(f"Welcome {hero.name}")
-    #Below here are the instructions to the game
-    print("")
-    save(player)
-    Continue()
