9b9eeb75559438f1efcd714e293faf547e2e8738
Godazss <52194386+Godazss@users.noreply.github.com>
2019-07-17 09:16:24 +0900

    Add files via upload

--- dungeon_conquest.py
+++ dungeon_conquest.py
@@ -1,4 +1,4 @@
-#save (Finished)
+#save(Finished)
 #smooth out battle system 
     #read player level
         #if player reaches level 5
@@ -63,19 +63,204 @@ class Hero:
         self.attack = [(5 + (5*self.str) + (5*self.dex)),(10 + (5*self.str) + (5*self.dex))]
         self.armour = [(1 + (5*self.str)), (10 + (5*self.str))]
         self.evasion = [(1 + (5*self.dex)), (10 + (5*self.dex))]
-        self.skills = ["Tackle"]
+        self.skills = ["Attack"]
+        self.sp = 40 + (10*self.dex)
+        self.all_skills = ["Attack", "Thrust", "Armour_Breaker", "Tendon_Cut", "Berserker_Slash", "Sacrifice",
+                           "Sexy_Wink", "Glob_of_Glue", "Armour_Up","Evasive_Boost", "Heal" ]
+
+# the following class methods will be used as skills, it is clumsy, but difficult to make interesting skills otherwise
+##### skill start #####################################################
+
+    def Attack(self, target):
+        
+    #calculates damage
+        attack_name = "Attack"
+        damage = random.randint(self.attack[0], self.attack[1]) - random.randint(target.armour[0], target.armour[1])
+    #if armour is higher than damage, damage = 0
+        if damage < 0:
+            damage = 0
+    #calculates if enemy dodged
+        if random.randint(target.evasion[0], target.evasion[1]) >= random.randint(0,100):
+            print(f"\n {player.name} uses {attack_name} on {target.name}.")
+            print(f"{target.name} dodged the attack!")
+        else:
+            print(f"\n{player.name} uses {attack_name} on {target.name}.")
+            print(f"{target.name} took {damage} damage!")
+            target.health = target.health - damage
+
+    def Thrust(self,target):
+        attack_name = "Thrust"
+        damage = random.randint(self.attack[0], self.attack[1]) - random.randint(target.armour[0], target.armour[1] - self.attack[0])
+        if damage < 0:
+            damage = 0
+        if random.randint(target.evasion[0], target.evasion[1]) >= random.randint(0,100):
+            
+            
+            print(f"\n {self.name} uses {attack_name} on {target.name}.")
+            print(f"{target.name} dodged the attack!")
+        else:
+            print(f"\n{self.name} uses {attack_name} on {target.name}.")
+            print(f"{target.name}'s armour was pierced for {damage} damage!")
+            target.health = target.health - damage
+
+    def Armour_Breaker(self,target):
+        attack_name = "Armour Breaker"
+        damage = random.randint(self.attack[0]*0.5, self.attack[1]*0.5) - random.randint(target.armour[0], target.armour[1])
+        if damage < 0:
+            damage = 0
+        if random.randint(target.evasion[0], target.evasion[1]) >= random.randint(0,100):
+            
+            print(f"\n {self.name} uses {attack_name} on {target.name}.")
+            print(f"{target.name} dodged the attack!")
+        else:
+            
+            print(f"\n{self.name} uses {attack_name} on {target.name}.")
+            print(f"{target.name}'s armour was thrashed for {damage} damage!")
+            target.health = target.health - damage
+            target.armour[1] = target.armour[1] - damage
+
+    def Tendon_Cut(self,target):
+        attack_name = "Tendon Cut"
+        damage = random.randint(self.attack[0]*0.5, self.attack[1]*0.5) - random.randint(target.armour[0], target.armour[1])
+        if damage < 0:
+            damage = 0
+        if random.randint(target.evasion[0], target.evasion[1]) >= random.randint(0,100):
+            
+            print(f"\n {self.name} uses {attack_name} on {target.name}.")
+            print(f"{target.name} dodged the attack!")
+        else:
+            
+            print(f"\n{self.name} uses {attack_name} on {target.name}.")
+            print(f"{target.name}'s tendons were cut for {damage} damage!")
+            target.health = target.health - damage
+            target.evasion[1] = target.evasion[1] * 0.80
+
+    def Berserker_slash(self,target):
+        attack_name = "Beserker Slash"
+        damage = random.randint(self.attack[0]*3, self.attack[1]*3) - random.randint(target.armour[0], target.armour[1])
+        if damage < 0:
+            damage = 0
+        if random.randint(target.evasion[0], target.evasion[1]) >= random.randint(0,100):
+            
+            print(f"\n {self.name} uses {attack_name} on {target.name}.")
+            print(f"{target.name} dodged the attack!")
+        else:
+            
+            print(f"\n{self.name} uses {attack_name} on {target.name}.")
+            print(f"{target.name} was brutally slashed for {damage} damage, but {self.name} was hurt by the attack!")
+            self.health -= (damage/5)
+            target.health = target.health - damage
+
+    def Sacrifice(self,target):
+        attack_name = "Sacrifice"
+        damage = random.randint(self.attack[0]*10, self.attack[1]*10) - random.randint(target.armour[0], target.armour[1])
+        if damage < 0:
+            damage = 0
+        if random.randint(target.evasion[0], target.evasion[1]) >= random.randint(60,100):
+            
+            print(f"\n {self.name} uses {attack_name} on {target.name}.")
+            print(f"{target.name} dodged the attack!")
+        else:
+            
+            print(f"\n{self.name} uses {attack_name} on {target.name}.")
+            print(f"""{self.name} knows the true meaning of sacrifice"\
+                  {target.name} took {damage} damage in an all out attack, but {self.name} was severly hurt by the attack!""")
+            self.health -= (self.health * 0.5)
+            target.health = target.health - damage
+
+    def Sexy_Wink(self,target):
+        attack_name = "Sexy Wink"
+        target.armour[1] = target.armour[1] * 0.6
+        self.evasion[1] = self.evasion[1] * 1.05
+        print(f"\n {self.name} uses {attack_name} on {target.name}.")
+        print(f"{target.name} gets the message and strips of some armour, {self.name} feels swifty")
+
+    def Glob_of_Glue(self,target):
+        attack_name = "Glob of Glue"
+        target.evasion[1] = target.evasion[1] * 0.6
+        target.attack[1] = target.attack[1] * 1.1
+        print(f"\n {self.name} uses {attack_name} on {target.name}.")
+        print(f"{target.name} is stuck, but {target.name} also feels annoyed")
+               
+
+    def Armour_Up(self):
+        attack_name = "Armour Up"
+        self.armour = [self.armour[0] * 1.5,self.armour[1] * 1.5] #Maybe boosts are too powerful, but minimum boost can be removed if so.
+        print(f"\n{self.name} uses {attack_name} and gets more armour")
+
+    def Evasive_Boost(self):
+        attack_name = "Evasive Boost"
+        self.evasion = [self.evasion[0] * 1.2,self.evasion[1] * 1.2] #Maybe boosts are too powerful, but minimum boost can be removed if so.
+        print(f"\n{self.name} uses {attack_name} and feels swifty.")
+
+    def Heal(self):
+        attack_name = "Heal"
+        base_stats = open_hero()
+        heal = base_stats.health * 0.20
+        if (heal + self.health) > base_stats.health:
+            self.health = base_stats.health
+        else:
+            self.health += heal
+        print(f"\n{self.name} uses {attack_name} and heals for {heal} HP.")
+
+
+##### skill end #########################################################
+    def Level_Up(self):
+        if self.experience >= 30:
+            self.level = self.level + 1
+            print(f"Player leveled up! Your level is now {self.level}")
+            print("Enter 1 for one point in strength")
+            print("Enter 2 for one point in dexterity")
+            x = int(input())
+            if x == 1:
+                print(f"Your strength went up from {self.str} to {self.str + 1}!")
+                self.str = self.str + 1
+            if x == 2:
+                print(f"Your dexterity went up from {self.dex} to {self.dex + 1}!")
+                self.str = self.dex + 1
+            choices = []
+            while len(choices) < 2:
+                choice = random.choice(self.all_skills)
+                if choice in self.skills or choice in choices:
+                    continue
+                else:
+                    choices.append(choice)
+            
+            print("You can learn new skills!")
+            user_choice = int(input(f"Pick 1:{choices[0]} or 2:{choices[1]}: "))-1
+            if len(self.skills)>=4:
+                userin = input("You can only have four skills, do you want to remove one?: ").lower()
+                if userin == "yes":
+                    remover = int(input(f"""Which skill do you want removed: 1:{self.skills[0]}, 2:{self.skills[1]}, 3:{self.skills[]} or"\
+                                    4:{self.skills[3]}: """))-1
+                    del(self.skills[remover])
+                    self.skills.append(choices[user_choice])
+                else:
+                    continue
+            else:
+                     
+                self.skills.append(choices[user_choice])
+            
+            self.experience = 0
+
+            
+        
+        
+        
+        
+    
 
         
 
 # monster class, (Simon, Aaron)
-class Monster:
+class Monster(Hero):
     def __init__(self, name):
         self.name = name
         self.health = 50
         self.attack = [1,10]
         self.armour = [1,10]
         self.evasion = [1,10]
-
+        self.sp = 500
         self.skills = ["m_Attack"]
 
 
@@ -125,6 +310,7 @@ def CharacterData(hero):
     print(f"Attack: {hero.attack[0]}" + "-" + f"{hero.attack[1]}")
     print(f"Armour: {hero.armour[0]}" + "-" + f"{hero.armour[1]}")
     print(f"Evasion: {hero.evasion[0]}" + "-" + f"{hero.evasion[1]}")
+    print(f"SP: {hero.sp}")
 
     print(f"Strength: {hero.str}")
     print(f"Dexterity: {hero.dex}")
@@ -156,6 +342,7 @@ def save(hero):
     hero_info["attack"] = hero.attack
     hero_info["armour"] = hero.armour
     hero_info["evasion"] = hero.evasion
+    hero_info["sp"] = hero.sp
     hero_info["str"] = hero.str
     hero_info["dex"] = hero.dex
     hero_info["skills"] = hero.skills
@@ -172,6 +359,7 @@ def savequit(hero):
     hero_info["attack"] = hero.attack
     hero_info["armour"] = hero.armour
     hero_info["evasion"] = hero.evasion
+    hero_info["sp"] = hero.sp
     hero_info["str"] = hero.str
     hero_info["dex"] = hero.dex
     hero_info["skills"] = hero.skills
@@ -191,6 +379,7 @@ def open_hero():
         hero.attack = hero_data["attack"]
         hero.armour = hero_data["armour"]
         hero.evasion = hero_data["evasion"]
+        hero.sp = hero_data["sp"]
         hero.str = hero_data["str"]
         hero.dex = hero_data["dex"]
         hero.skills = hero_data["skills"]
@@ -206,6 +395,7 @@ def open_midboss():
         midboss.attack = monster_data["attack"]
         midboss.armour = monster_data["armour"]
         midboss.evasion = monster_data["evasion"]
+        midboss.sp = monster_data["sp"]
         midboss.skills = monster_data["skills"]
     return midboss
 
@@ -219,6 +409,7 @@ def open_finalboss():
         finalboss.attack = monster_data["attack"]
         finalboss.armour = monster_data["armour"]
         finalboss.evasion = monster_data["evasion"]
+        finalboss.sp = monster_data["sp"]
         finalboss.skills = monster_data["skills"]
     return finalboss
 
@@ -230,6 +421,7 @@ def midboss():
     monster_info["attack"] = [1,100]
     monster_info["armour"] = [1,100]
     monster_info["evasion"] = [1,50]
+    monster_info["sp"] = 500
     monster_info["skills"] = ["m_Tackle"]
     with open("midboss.json", "w") as outfile:
         json.dump(monster_info, outfile, indent=3)
@@ -242,6 +434,7 @@ def finalboss():
     monster_info["attack"] = [1,200]
     monster_info["armour"] = [1,200]
     monster_info["evasion"] = [1,100]
+    monster_info["sp"] = 500
     monster_info["skills"] = ["m_Tackle"]
     with open("finalboss.json", "w") as outfile:
         json.dump(monster_info, outfile, indent=3)
@@ -305,7 +498,7 @@ def battle():
         #since eval() is being used, this is for security reasons
         if y >=0 and y <= 10:
             try:
-                eval(f"{player.skills[y]}(player, monster)")
+                eval(f"{player.skills[y]}(monster)")
             except:
                 print("incorrect input")
                 continue
@@ -313,29 +506,23 @@ def battle():
             print("incorrect input")
             continue
         m_Attack(monster, player)
-        print(f"Player HP: {player.health}")
-        print(f"Monter HP: {monster.health}")
+        player.sp += 10
+        print(f"Player HP: {player.health}\nPlayer SP: {player.sp}")
+        print(f"Monster HP: {monster.health}")
+        
     if player.health <= 0:
         print("You died")
         print("Your high score is _____")
         print("New Game? y/n")
     else:
-        print(f"You defeated {monster.name} and gained 10 experience")
-        origin.experience = origin.experience + 10
-        if origin.experience >= 30:
-            origin.level = origin.level + 1
-            print(f"Player leveled up! Your level is now {origin.level}")
-            print("Enter 1 for one point in strength")
-            print("Enter 2 for one point in dexterity")
-            x = input()
-            if x == 1:
-                print(f"Your strength went up from {origin.str} to {origin.str + 1}!")
-                origin.str = origin.str + 1
-            if x == 2:
-                print(f"Your dexterity went up from {origin.dex} to {origin.dex + 1}!")
-                origin.str = origin.dex + 1
+        print(f"You defeated {monster.name} and gained 30 experience")
+        origin.experience = origin.experience + 30
+        origin.Level_Up()
+       
         #save also continues the game
         save(origin)
+
+    
 
         
     
@@ -360,22 +547,22 @@ def battle():
 
 
 
-#Simulates attack, (Simon, Aaron)
-def Tackle(player, target):
-    #calculates damage
-    attack_name = "Tackle"
-    damage = random.randint(player.attack[0], player.attack[1]) - random.randint(target.armour[0], target.armour[1])
-    #if armour is higher than damage, damage = 0
-    if damage < 0:
-        damage = 0
-    #calculates if enemy dodged
-    if random.randint(target.evasion[0], target.evasion[1]) >= random.randint(0,100):
-        print(f"\n {player.name} uses {attack_name} on {target.name}.")
-        print(f"{target.name} dodged the attack!")
-    else:
-        print(f"\n{player.name} uses {attack_name} on {target.name}.")
-        print(f"{target.name} took {damage} damage!")
-        target.health = target.health - damage
+###Simulates attack, (Simon, Aaron)
+##def Attack(player, target):
+##    #calculates damage
+##    attack_name = "Attack"
+##    damage = random.randint(player.attack[0], player.attack[1]) - random.randint(target.armour[0], target.armour[1])
+##    #if armour is higher than damage, damage = 0
+##    if damage < 0:
+##        damage = 0
+##    #calculates if enemy dodged
+##    if random.randint(target.evasion[0], target.evasion[1]) >= random.randint(0,100):
+##        print(f"\n {player.name} uses {attack_name} on {target.name}.")
+##        print(f"{target.name} dodged the attack!")
+##    else:
+##        print(f"\n{player.name} uses {attack_name} on {target.name}.")
+##        print(f"{target.name} took {damage} damage!")
+##        target.health = target.health - damage
 
 
 #Simulates Monster Attack, (Simon, Aaron)
@@ -401,7 +588,6 @@ x.name = "Arnold"
 y = Monster("Apple")
 save(x)
 open_hero()
-
 m = "Tackle"
 z = eval(f"{m}(x,y)")
 """
